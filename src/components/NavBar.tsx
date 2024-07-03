9935f6c96abfa75f47cbc4fcc268106929d8b7d8
manish01101 <manishchotu4@gmail.com>
2024-07-03 23:52:51 +0530

    color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { HStack, Image } from "@chakra-ui/react"
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
     return (
-        <HStack>
+        <HStack justifyContent={'space-between'} padding={'10px'}>
             <Image src={'https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1719964800&semt=sph'} boxSize={'50px'}/>
-            <Text>NavBAR </Text>
+            <ColorModeSwitch />
         </HStack>
     )
 }
