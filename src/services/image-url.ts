c8561283b96f9aa4a32b2d678b970648d3fab103
manish01101 <manishchotu4@gmail.com>
2024-07-05 18:16:46 +0530

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,7 @@
 
 const getCroppedImageUrl = (url: string) => {
 
-  if(!url) return '';
+  if(!url) return 'https://i.pinimg.com/736x/5f/b9/b8/5fb9b88dd66004e2d21ca4ca50f341dd.jpg';
   
   const index = url.indexOf('media/') + 'media/'.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
